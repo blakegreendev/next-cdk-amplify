4c2bc84e36da2da500ad92fc103dcc62076ad03a
Blake Green <blakegreen@msn.com>
2021-06-22 19:17:20 -0400

    add passwordless

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,14 +1,71 @@
 import * as amp from "@aws-cdk/aws-amplify";
-import { BuildSpec } from "@aws-cdk/aws-codebuild";
 import { UserPool, CfnUserPoolClient, CfnIdentityPool } from "@aws-cdk/aws-cognito";
-import { ManagedPolicy, Role, ServicePrincipal } from "@aws-cdk/aws-iam";
+import { ManagedPolicy, PolicyStatement, Role, ServicePrincipal } from "@aws-cdk/aws-iam";
+import { NodejsFunction } from "@aws-cdk/aws-lambda-nodejs";
+import * as logs from "@aws-cdk/aws-cloudwatch";
 import { Construct, Stack, StackProps, SecretValue, Duration, CfnOutput } from "@aws-cdk/core";
 
 export class InfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
+    ///////////// Auth Lambda
+    // Create role
+    const authRole = new Role(this, "AuthRole", {
+      assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
+    });
+    authRole.addToPolicy(new PolicyStatement({
+      resources: ['*'],
+      actions: [
+        'ses:SendEmail', 
+        'cognito-idp:AdminUpdateUserAttributes']
+    }))
+    // Create lambda functions
+    const emailAddress = "blakegreen@msn.com"
+    const createAuthFunction = new NodejsFunction(this, 'CreateAuthFunction', {
+      entry: 'functions/passwordless/create-auth-challenge.ts', 
+      handler: 'CreateAuthChallengeTriggerHandler', 
+      role: authRole,
+      environment: {
+        'SES_FROM_ADDRESS':emailAddress
+      }
+    });
+    const preSignUpFunction = new NodejsFunction(this, 'PreSignUpFunction', {
+      entry: 'functions/passwordless/pre-signup.ts', 
+      handler: 'PreSignUpTriggerHandler', 
+      role: authRole,
+      environment: {
+        'SES_FROM_ADDRESS':emailAddress
+      }
+    });
+    const defineAuthFunction = new NodejsFunction(this, 'DefineAuthFunction', {
+      entry: 'functions/passwordless/define-auth-challenge.ts', 
+      handler: 'DefineAuthChallengeTriggerHandler', 
+      role: authRole,
+      environment: {
+        'SES_FROM_ADDRESS':emailAddress
+      }
+    });
+    const postAuthFunction = new NodejsFunction(this, 'PostAuthFunction', {
+      entry: 'functions/passwordless/post-authentication.ts', 
+      handler: 'PostAuthenticationTriggerHandler', 
+      role: authRole,
+      environment: {
+        'SES_FROM_ADDRESS':emailAddress
+      }
+    });
+    const verifyAuthFunction = new NodejsFunction(this, 'VerifyAuthFunction', {
+      entry: 'functions/passwordless/verify-auth-challenge-response.ts', 
+      handler: 'VerifyAuthChallengeResponseTriggerHandler', 
+      role: authRole,
+      environment: {
+        'SES_FROM_ADDRESS':emailAddress
+      }
+    });
+    ///////////// Cognito
+    // Create user pool
     const userPool = new UserPool(this, "UserPool", {
+      selfSignUpEnabled: true,
+      //signInAliases: {email: true},
       passwordPolicy: {
         requireDigits: false,
         requireLowercase: false,
@@ -16,8 +73,16 @@ export class InfraStack extends Stack {
         requireUppercase: false, 
         minLength: 6,
         tempPasswordValidity: Duration.days(7)
+      },
+      lambdaTriggers: {
+        createAuthChallenge: createAuthFunction,
+        preSignUp: preSignUpFunction,
+        defineAuthChallenge: defineAuthFunction,
+        postAuthentication: postAuthFunction,
+        verifyAuthChallengeResponse: verifyAuthFunction
       }
     })
+    // Create user pool client
     const userPoolClient = new CfnUserPoolClient(this, "UserPoolClient", {
       userPoolId: userPool.userPoolId,
       generateSecret: false,
@@ -33,6 +98,8 @@ export class InfraStack extends Stack {
         providerName: userPool.userPoolProviderName
       }]
     })
+
+    ///////////// Amplify
     const role = new Role(this, "MyRole", {
       assumedBy: new ServicePrincipal("amplify.amazonaws.com"),
     });
@@ -67,6 +134,7 @@ export class InfraStack extends Stack {
     });
     domain.mapRoot(main); // map master branch to domain root
 
+    ////////////////// Outputs
     new CfnOutput(this, "IdentityPoolOut", {value: identityPool.ref})
     new CfnOutput(this, "UserPoolIdOut", {value: userPool.userPoolId})
     new CfnOutput(this, "UserPoolClientIdOut", {value: userPoolClient.ref})
